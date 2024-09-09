cb7cdcf574292d6609d3616e0a2e9353f180d174
juanmoncayo55 <juansebastianmoriones@unimayor.edu.co>
2024-09-09 15:06:28 -0500

    Agregando lista de tiendas para seguir

--- views/HomeDashboard.js
+++ views/HomeDashboard.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { View, StyleSheet, ImageBackground, Image, FlatList } from 'react-native';
-import { Box, Text, Heading, ScrollView, Stack, Button, HStack, VStack, Flex, Pressable} from "native-base";
+import { Box, Text, Heading, ScrollView, Stack, Button, HStack, VStack, Flex, Pressable, AspectRatio, Center} from "native-base";
 import globalStyles from "../styles/globalStyles";
 import CardProduct from "../components/CardProduct";
 
@@ -108,7 +108,7 @@ const HomeDashboard = () => {
 	      </View>
 	      <ScrollView
 	  			horizontal
-	  			contentContainerStyle={{display: "flex", columnGap: 10, paddingHorizontal: "5%", paddingRight: 120}}
+	  			contentContainerStyle={{display: "flex", columnGap: 10, paddingHorizontal: "3%", paddingRight: 120}}
 	  			mb="9"
 	  		>
 	      	<CardProduct />
@@ -119,7 +119,7 @@ const HomeDashboard = () => {
 	      	<CardProduct />
 	  		</ScrollView>
 
-	      
+
 	    	<View style={styles.contenido}>
 	      	<Stack my="3">
 	      		<VStack display="flex" flexDirection="row" justifyContent="space-between" alignItems="center">
@@ -136,7 +136,7 @@ const HomeDashboard = () => {
 	      </View>
 	  		<ScrollView
 	  			horizontal
-	  			contentContainerStyle={{display: "flex", columnGap: 10, paddingHorizontal: "5%", paddingRight: 120}}
+	  			contentContainerStyle={{display: "flex", columnGap: 10, paddingHorizontal: "3%", paddingRight: 120}}
 	  			mb="9"
 	  		>
 	      	<CardProduct />
@@ -147,11 +147,66 @@ const HomeDashboard = () => {
 	      	<CardProduct />
 	  		</ScrollView>
 
+	  		<View style={[styles.contenido, {marginHorizontal: 0,width: "100%"}]}>
+	      	<Stack py="5" style={{paddingHorizontal: "5%", width: "100%", height: 170, backgroundColor: "#33907C"}}>
+	      		<VStack display="flex" flexDirection="row" justifyContent="space-between" alignItems="center">
+	      			<HStack>
+	      				<Heading color="#FFF" fontSize={22}>Store to follow</Heading>
+	      			</HStack>
+	      			<HStack>
+	      				<Button style={{backgroundColor: "#FFF"}} variant="outline" rounded="full" px={7} py={0.5}>
+			      			<Text color="#33907C" fontSize="md">View all</Text>
+			      		</Button>
+	      			</HStack>
+	      		</VStack>
+	      	</Stack>
+	      	<View style={{position: "relative", top: -100, marginBottom: -80}}>
+		      <ScrollView
+		  			horizontal
+		  			contentContainerStyle={{display: "flex", columnGap: 10,paddingRight: 280, paddingHorizontal: "3%"}}
+		  		>
+		      	{
+		      		[1,2,3,4].map(item => (
+		      			<Box maxW="55%" rounded="lg" overflow="hidden" borderColor="coolGray.200" borderWidth="1">
+		      				<Box>
+		      					<AspectRatio w="100%" ratio={16 / 10}>
+							        <Image source={{
+								        uri: "https://www.holidify.com/images/cmsuploads/compressed/Bangalore_citycover_20190613234056.jpg"
+								      }} alt="image" />
+							      </AspectRatio>
+		      				</Box>
+		      				<Stack p="3" space={3} bgColor="white">
+		      					<Box flexDirection="row" justifyContent="center" alignItems="center">
+			      					<Center
+						        		style={styles.userFirstLetter}
+						        		rounded="full"
+						        		position="absolute"
+						        		top="-45"
+						        	>
+						        		<Text color="white" fontSize="5xl" fontWeight="semibold" style={{lineHeight: 65}}>T</Text>
+						        	</Center>
+					        	</Box>
+		      					<Heading size="md" mt="3" mb="4" color="#4A4A4A" fontWeight="normal" textAlign="center">
+						          Tridly Store
+						        </Heading>
+						        <Box flexDirection="row" justifyContent="center">
+						        	<Button style={{backgroundColor: "#33907C"}} variant="outline" rounded="full" px={7} py={0.5}>
+						      			<Text color="#FFF" fontSize="md">Follow</Text>
+						      		</Button>
+						        </Box>
+		      				</Stack>
+		      			</Box>
+		      		))
+		      	}
+		  		</ScrollView>
+		  		</View>
+	      </View>
+
       </ScrollView>
 		</View>
 	)
 }
-
+//, transform: [{translateY: 100}]
 const styles = StyleSheet.create({
 	contenido: {
     width: "90%",
@@ -172,10 +227,13 @@ const styles = StyleSheet.create({
   },
   userFirstLetter: {
   	backgroundColor: "#33907C",
-  	width: 25,
-  	height: 25,
+  	width: 65,
+  	height: 65,
   	alignItems: "center",
-  	justifyContent: "center"
+  	justifyContent: "center",
+  	borderWidth: 2,
+  	borderColor: "#FFF",
+  	borderStyle: "solid"
   }
 })
 
