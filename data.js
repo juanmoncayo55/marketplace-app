d4a056f9c0d311a44feb5ae0b150f38b24604ff6
juanmoncayo55 <juansebastianmoriones@unimayor.edu.co>
2024-09-09 21:48:29 -0500

    Simulando datos para las categorias de alimentos.

---
+++ data.js
@@ -0,0 +1,374 @@
+const data = {
+	"data": {
+		"beverages": [
+			{
+				"name": "Strawberry Punch",
+				"price": 25,
+				"image":require("./images/beveragesp.png"),
+				"user": {
+					"name": "Tradly"
+				}
+			},
+			{
+				"name": "Lemonade",
+				"price": 25,
+				"image":require("./images/beveragesp.png"),
+				"user": {
+					"name": "Tradly"
+				}
+			},
+			{
+				"name": "Chocolate",
+				"price": 25,
+				"image":require("./images/beveragesp.png"),
+				"user": {
+					"name": "Tradly"
+				}
+			},
+			{
+				"name": "Whisky",
+				"price": 25,
+				"image":require("./images/beveragesp.png"),
+				"user": {
+					"name": "Tradly"
+				}
+			},
+			{
+				"name": "Chocolate Bakery",
+				"price": 25,
+				"image":require("./images/beveragesp.png"),
+				"user": {
+					"name": "Tradly"
+				}
+			},
+			{
+				"name": "Fruit Punch",
+				"price": 25,
+				"image":require("./images/beveragesp.png"),
+				"user": {
+					"name": "Tradly"
+				}
+			}
+		],
+		"Bread": [
+			{
+				"name": "Bread Chocolate",
+				"price": 25,
+				"image":require("./images/bread.png"),
+				"user": {
+					"name": "Tradly"
+				}
+			},
+			{
+				"name": "Circle Bakery",
+				"price": 25,
+				"image":require("./images/bread.png"),
+				"user": {
+					"name": "Tradly"
+				}
+			},
+			{
+				"name": "Cookies",
+				"price": 25,
+				"image":require("./images/bread.png"),
+				"user": {
+					"name": "Tradly"
+				}
+			},
+			{
+				"name": "Long Bread",
+				"price": 25,
+				"image":require("./images/bread.png"),
+				"user": {
+					"name": "Tradly"
+				}
+			},
+			{
+				"name": "Donut",
+				"price": 25,
+				"image":require("./images/bread.png"),
+				"user": {
+					"name": "Tradly"
+				}
+			},
+			{
+				"name": "Bread",
+				"price": 25,
+				"image":require("./images/bread.png"),
+				"user": {
+					"name": "Tradly"
+				}
+			},
+		],
+		"vegetables": [
+			{
+				"name": "Carrot",
+				"price": 25,
+				"image":require("./images/vegetablesp.png"),
+				"user": {
+					"name": "Tradly"
+				}
+			},
+			{
+				"name": "Cabbage",
+				"price": 25,
+				"image":require("./images/vegetablesp.png"),
+				"user": {
+					"name": "Tradly"
+				}
+			},
+			{
+				"name": "Tomato",
+				"price": 25,
+				"image":require("./images/vegetablesp.png"),
+				"user": {
+					"name": "Tradly"
+				}
+			},
+			{
+				"name": "Garlic",
+				"price": 25,
+				"image":require("./images/vegetablesp.png"),
+				"user": {
+					"name": "Tradly"
+				}
+			},
+			{
+				"name": "Tomato",
+				"price": 25,
+				"image":require("./images/vegetablesp.png"),
+				"user": {
+					"name": "Tradly"
+				}
+			},
+			{
+				"name": "Corn",
+				"price": 25,
+				"image":require("./images/vegetablesp.png"),
+				"user": {
+					"name": "Tradly"
+				}
+			}
+		],
+		"fruit": [
+			{
+				"name": "Avocado",
+				"price": 25,
+				"image":require("./images/fruitp.png"),
+				"user": {
+					"name": "Tradly"
+				}
+			},
+			{
+				"name": "Banana",
+				"price": 25,
+				"image":require("./images/fruitp.png"),
+				"user": {
+					"name": "Tradly"
+				}
+			},
+			{
+				"name": "Orange",
+				"price": 25,
+				"image":require("./images/fruitp.png"),
+				"user": {
+					"name": "Tradly"
+				}
+			},
+			{
+				"name": "Papaya",
+				"price": 25,
+				"image":require("./images/fruitp.png"),
+				"user": {
+					"name": "Tradly"
+				}
+			},
+			{
+				"name": "Pineapple",
+				"price": 25,
+				"image":require("./images/fruitp.png"),
+				"user": {
+					"name": "Tradly"
+				}
+			},
+			{
+				"name": "Watermelon",
+				"price": 25,
+				"image":require("./images/fruitp.png"),
+				"user": {
+					"name": "Tradly"
+				}
+			},
+		],
+		"egg": [
+			{
+				"name": "Brown egg",
+				"price": 25,
+				"image":require("./images/eggp.png"),
+				"user": {
+					"name": "Tradly"
+				}
+			},
+			{
+				"name": "Fresh Egg",
+				"price": 25,
+				"image":require("./images/eggp.png"),
+				"user": {
+					"name": "Tradly"
+				}
+			},
+			{
+				"name": "Bundle Egg",
+				"price": 25,
+				"image":require("./images/eggp.png"),
+				"user": {
+					"name": "Tradly"
+				}
+			},
+			{
+				"name": "Blue Egg",
+				"price": 25,
+				"image":require("./images/eggp.png"),
+				"user": {
+					"name": "Tradly"
+				}
+			},
+			{
+				"name": "Bird Egg",
+				"price": 25,
+				"image":require("./images/eggp.png"),
+				"user": {
+					"name": "Tradly"
+				}
+			},
+			{
+				"name": "Egg",
+				"price": 25,
+				"image":require("./images/eggp.png"),
+				"user": {
+					"name": "Tradly"
+				}
+			}
+		],
+		"frozen": [
+			{
+				"name": "Ice Cream",
+				"price": 25,
+				"image":require("./images/frozenp.png"),
+				"user": {
+					"name": "Tradly"
+				}
+			},
+			{
+				"name": "Manggo Ice",
+				"price": 25,
+				"image":require("./images/frozenp.png"),
+				"user": {
+					"name": "Tradly"
+				}
+			},
+			{
+				"name": "Strawberry Ice",
+				"price": 25,
+				"image":require("./images/frozenp.png"),
+				"user": {
+					"name": "Tradly"
+				}
+			},
+			{
+				"name": "Matcha",
+				"price": 25,
+				"image":require("./images/frozenp.png"),
+				"user": {
+					"name": "Tradly"
+				}
+			},
+			{
+				"name": "Grape Ice Cream",
+				"price": 25,
+				"image":require("./images/frozenp.png"),
+				"user": {
+					"name": "Tradly"
+				}
+			},
+			{
+				"name": "Frozen Bottle",
+				"price": 25,
+				"image":require("./images/frozenp.png"),
+				"user": {
+					"name": "Tradly"
+				}
+			}
+		],
+		"homeCare": [
+			{
+				"name": "Moisturizer",
+				"price": 25,
+				"image":require("./images/homecarep.png"),
+				"user": {
+					"name": "Tradly"
+				}
+			},
+			{
+				"name": "Vitamin Bundle",
+				"price": 25,
+				"image":require("./images/homecarep.png"),
+				"user": {
+					"name": "Tradly"
+				}
+			},
+			{
+				"name": "Shower Gel",
+				"price": 25,
+				"image":require("./images/homecarep.png"),
+				"user": {
+					"name": "Tradly"
+				}
+			},
+			{
+				"name": "Facial Wash",
+				"price": 25,
+				"image":require("./images/homecarep.png"),
+				"user": {
+					"name": "Tradly"
+				}
+			},
+			{
+				"name": "Onne Beauty",
+				"price": 25,
+				"image":require("./images/homecarep.png"),
+				"user": {
+					"name": "Tradly"
+				}
+			},
+			{
+				"name": "Fur Moisturozer",
+				"price": 25,
+				"image":require("./images/homecarep.png"),
+				"user": {
+					"name": "Tradly"
+				}
+			}
+		],
+		"petCare": [
+			{
+				"name": "Pet Snack",
+				"price": 25,
+				"image":require("./images/petcarep.png"),
+				"user": {
+					"name": "Tradly"
+				}
+			},
+			{
+				"name": "Potion Pet",
+				"price": 25,
+				"image":require("./images/petcarep.png"),
+				"user": {
+					"name": "Tradly"
+				}
+			},
+		]
+	}
+}
+
+export default data
